491deaf352bd087c6c5e3447dc71733d94738819
AmirHosein Rahimi <amirhoseinrahimi.work@gmail.com>
2022-09-04 17:28:28 +0430

    add: config

--- config/panel.php
+++ config/panel.php
@@ -1,5 +1,63 @@
 <?php
 
 return [
-    //
+
+    /*
+    |--------------------------------------------------------------------------
+    | Panel Title
+    |--------------------------------------------------------------------------
+    */
+
+    'title' => "پنل مدیریت",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Panel Logout Route
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the route name for logout button.
+    |
+    */
+
+    'logoutRoute' => 'logout',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Panel User Info
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the user info for panel.
+    | Get user info from auth()->user() if login.
+    | You can use multiple params like this: 'first_name,last_name', only for name.
+    | if not found any param, set null
+    |
+    */
+
+    'user' => [
+        'name' => 'name,lastname',
+        'side' => "email",
+        'image' => 'image',
+        'email' => "email",
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Panel Items
+    |--------------------------------------------------------------------------
+    |
+    | This option defines the items for panel.
+    | You can use [route name] or [url] for key.
+    | In params you can set name, icon, show, disabled.
+    | Icon is from fontawesome icon.
+    |
+    */
+
+    'items' => [
+        '/' => [
+            'name' => 'داشبورد',
+            'icon' => 'fa-light fa-home-lg-alt',
+            'show' => true,
+            'disabled' => false,
+        ],
+    ],
 ];
