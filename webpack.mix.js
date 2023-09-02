0092c4f35b05959d85b56dabd19015dcadd8c0ee
AmirHosein Rahimi <amirhoseinrahimi.work@gmail.com>
2023-09-02 14:53:13 +0330

    add: typographies

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,5 +6,9 @@ mix.options({
 
 mix.js('resources/js/app.js', 'resources/dist/js/app.js')
     .css('resources/css/app.css', 'resources/dist/css/app.css')
+
+    .css('resources/css/typographies/dana.css', 'resources/dist/css/typographies')
+    .css('resources/css/typographies/nunito.css', 'resources/dist/css/typographies')
+
     .css('resources/css/ltr.css', 'resources/dist/css/ltr.css')
     .css('resources/css/rtl.css', 'resources/dist/css/rtl.css');
