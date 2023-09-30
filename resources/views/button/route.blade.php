e29a4a3d26f249297742ec9ceab43724b0820649
AmirHosein Rahimi <amirhoseinrahimi.work@gmail.com>
2023-09-30 17:50:58 +0330

    Update route.blade.php

--- resources/views/button/route.blade.php
+++ resources/views/button/route.blade.php
@@ -1,4 +1,6 @@
-@props($neededs = T0team\LaravelPanel\Helper::needed($row ?? [], $button->route['needed']))
+@php
+    $neededs = T0team\LaravelPanel\Helper::needed($row ?? [], $button->route['needed']);
+@endphp
 
 <a href="{!! route($button->route['name'], $neededs, false) !!}"
     class="btn btn-{{ $button->outLine ? 'outline-' : '' }}{{ $button->color }} btn-{{ $button->size }} {{ $button->disabled ? 'disabled' : '' }}"
